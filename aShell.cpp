c4c812f350e0836907cc966dd40bd189ecfc1526
Danisa-R <contactdanisa@gmail.com>
2023-09-27 18:56:48 -0700

    "Fixed typo w/ execve and changed PATH to bin"

--- aShell.cpp
+++ aShell.cpp
@@ -18,19 +18,21 @@ void read_command(char cmd[], char *par[]) {
         if (count == 1) {
             return;
         }
-        pch = strtok(line, "\n");
+        pch = strtok(line, "\n"); // break the lines into tokens (words)
 
+        // parse the line into words
         while (pch != NULL)
         {
             array[i++] = strdup(pch);
             pch = strtok(NULL, "\n");
         }
-
+        // first word is the command
         strcpy(cmd, array[0]);
 
+        // others are parameters
         for (int j = 0; j < i; j++) {
             par[j] = array[j];
-            par[i] = NULL;
+            par[i] = NULL; // NULL-terminate the parameter list
         }
     }
 }
@@ -39,7 +41,7 @@ void type_prompt() {
     static int first_time = 1;
     if (first_time) {
         const char* CLEAR_SCREEN_ANSI = "\e[1;1H\e[2J";
-        write(STDERR_FILENO, CLEAR_SCREEN_ANSI, 12);
+        write(STDOUT_FILENO, CLEAR_SCREEN_ANSI, 12);
         first_time = 0;
     }
 
@@ -49,16 +51,16 @@ void type_prompt() {
 int main() {
     char cmd[100], command[100], *parameters[20];
 
-    char *envp[] = { (char *) "PATH=C:\\Windows\\System32", 0 };
+    char *envp[] = { (char *) "PATH=/bin", 0 };
     while(1) {
         type_prompt();
         read_command(command, parameters);
         if (fork() != 0) {
             wait(NULL);
         } else {
-            strcpy(cmd, "C:\\Windows\\System32");
+            strcpy(cmd, "/bin/");
             strcpy(cmd, command);
-            execvp(cmd, parameters, envp);
+            execve(cmd, parameters, envp);
         }
         if (strcmp(command, "exit") == 0) {
             break;
